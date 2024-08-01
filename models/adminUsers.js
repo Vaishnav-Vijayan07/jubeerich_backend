4461ee972e38d3ee4d721a4da94087fd32d56de8
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-08-01 11:09:23 +0530

    modified admin users schema

--- models/adminUsers.js
+++ models/adminUsers.js
@@ -43,6 +43,15 @@ module.exports = (sequelize, Sequelize) => {
       },
       onDelete: "SET NULL",
     },
+    country_id: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: "countries", // name of the target table
+        key: "id",
+      },
+      onDelete: "SET NULL",
+    },
     updated_by: {
       type: Sequelize.INTEGER,
     },
