a5ac461a8f3f9db9fcfd387f400b3e799b6d1753
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-08-05 11:23:55 +0530

    added updated_by field

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -464,107 +464,6 @@ exports.getAllLeads = async (req, res) => {
   }
 };
 
-
-// exports.getAllLeads = async (req, res) => {
-//   const cre_id = req.userDecodeId;
-
-//   try {
-//     const userPrimaryInfos = await UserPrimaryInfo.findAll({
-//       where: {
-//         [db.Sequelize.Op.or]: [
-//           { assigned_cre_tl: cre_id },
-//           { created_by: cre_id },
-//           { counsiler_id: cre_id },
-//           { assigned_cre: cre_id },
-//         ],
-//         is_deleted: false,
-//       },
-//       include: [
-//         {
-//           model: db.leadCategory,
-//           as: "category_name",
-//           attributes: ["category_name"],
-//         },
-//         {
-//           model: db.leadSource,
-//           as: "source_name",
-//           attributes: ["source_name"],
-//         },
-//         {
-//           model: db.leadChannel,
-//           as: "channel_name",
-//           attributes: ["channel_name"],
-//         },
-//         {
-//           model: db.country,
-//           as: "preferredCountries", // Use the alias defined in associations
-//           attributes: ["country_name", "id"], // Include the ID attribute
-//           through: { attributes: [] }, // Exclude attributes from join table
-//         },
-//         {
-//           model: db.officeType,
-//           as: "office_type_name",
-//           attributes: ["office_type_name"],
-//         },
-//         {
-//           model: db.region,
-//           as: "region_name",
-//           attributes: ["region_name"],
-//           required: false,
-//         },
-//         {
-//           model: db.adminUsers,
-//           as: "counsiler_name",
-//           attributes: ["name"],
-//           required: false,
-//         },
-//         {
-//           model: db.branches,
-//           as: "branch_name",
-//           attributes: ["branch_name"],
-//           required: false,
-//         },
-//       ],
-//     });
-
-//     const formattedUserPrimaryInfos = userPrimaryInfos.map((info) => {
-//       const preferredCountries = info.preferredCountries.map((country) => ({
-//         country_name: country.country_name,
-//         id: country.id,
-//       }));
-
-//       return {
-//         ...info.toJSON(),
-//         category_name: info.category_name
-//           ? info.category_name.category_name
-//           : null,
-//         source_name: info.source_name ? info.source_name.source_name : null,
-//         channel_name: info.channel_name ? info.channel_name.channel_name : null,
-//         preferredCountries: preferredCountries,
-//         office_type_name: info.office_type_name
-//           ? info.office_type_name.office_type_name
-//           : null,
-//         region_name: info.region_name ? info.region_name.region_name : null,
-//         counsiler_name: info.counsiler_name ? info.counsiler_name.name : null,
-//         branch_name: info.branch_name ? info.branch_name.branch_name : null,
-//       };
-//     });
-
-//     res.status(200).json({
-//       status: true,
-//       message: "User primary info retrieved successfully",
-//       formattedUserPrimaryInfos,
-//       allCres: null,
-//     });
-//   } catch (error) {
-//     console.error(`Error fetching user primary info: ${error}`);
-//     res.status(500).json({
-//       status: false,
-//       message: "Internal server error",
-//     });
-//   }
-// };
-
 exports.getLeadsByCreatedUser = async (req, res) => {
   try {
     const userId = req.userDecodeId;
@@ -985,7 +884,7 @@ exports.assignCres = async (req, res) => {
 
         // Update assigned_cre for the user
         await db.userPrimaryInfo.update(
-          { assigned_cre: cre_id },
+          { assigned_cre: cre_id, updated_by: userId },
           { where: { id: user_id }, transaction }
         );
       })
@@ -1065,7 +964,7 @@ exports.autoAssign = async (req, res) => {
       }
 
       return UserPrimaryInfo.update(
-        { assigned_cre: currentCre },
+        { assigned_cre: currentCre, updated_by: userId },
         { where: { id }, transaction }
       );
     });
