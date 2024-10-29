5ebf5b916990c5d12ec85731adf74aa4a0d8a0a1
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-10-29 12:17:18 +0530

    added transaction

--- controller/lead_import_controller.js
+++ controller/lead_import_controller.js
@@ -219,13 +219,10 @@ exports.bulkUpload = async (req, res) => {
         if (preferredCountries.length > 0) {
           const studyPreferences = await Promise.all(
             preferredCountries.map(async (countryId) => {
-              return await db.studyPreference.create(
-                {
-                  userPrimaryInfoId: userId,
-                  countryId,
-                },
-                { transaction } // Pass the transaction here inside the create call
-              );
+              return await db.studyPreference.create({
+                userPrimaryInfoId: userId,
+                countryId,
+              });
             })
           );
         }
