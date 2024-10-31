42f48dcb08867fc000052d905762edddf3f4d840
afsalmern <afsal@intersmart.in>
2024-10-31 12:04:22 +0530

    added the association for eligibility check

--- models/eligibility_checks.js
+++ models/eligibility_checks.js
@@ -1,36 +1,49 @@
 module.exports = (sequelize, Sequelize) => {
-    const EligibilityChecks = sequelize.define("eligibility_checks", {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+  const EligibilityChecks = sequelize.define("eligibility_checks", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    application_id: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: "application_details",
+        key: "id",
       },
-      availability_check: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
+    },
+    availability_check: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    campus_check: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    entry_requirement_check: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    quantity_check: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    quality_check: {
+      type: Sequelize.JSONB,
+      allowNull: true,
+      defaultValue: {
+        formatting: false,
+        checks: false,
+        clarity: false,
       },
-      campus_check: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-      entry_requirement_check: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-      quantity_check: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-      quality_check: {
-        type: Sequelize.JSONB,
-        allowNull: true,
-      },
-      immigration_check: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-    });
-  
-    return EligibilityChecks;
-  };
-  
+    },
+    immigration_check: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+
+  return EligibilityChecks;
+};
+
