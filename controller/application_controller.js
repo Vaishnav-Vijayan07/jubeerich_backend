92df1a53374cba8022ba18287f6b01e7c930d2ec
afsalmern <afsal@intersmart.in>
2024-10-31 17:37:02 +0530

    update in application controller

--- controller/application_controller.js
+++ controller/application_controller.js
@@ -2,109 +2,106 @@ const db = require("../models");
 const sequelize = db.sequelize;
 const { Sequelize } = require("sequelize");
 const types = {
-  education : 'education',
-  visa : 'visa'
-}
+  education: "education",
+  visa: "visa",
+};
 
 exports.getApplicationById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const existApplication = await db.application.findByPk(id);
+    const existApplication = await db.application.findByPk(id);
 
-        if (!existApplication) {
-            throw new Error("Application not found");
-        }
-
-        const [assigned_user, studyPreferDetails] = await Promise.all([
-            existApplication.getApplication({ attributes: ["id", "name"] }),
-            existApplication.getStudyPreferenceDetails(
-                {
-                    attributes: ["id", "intakeYear", "intakeMonth", "application_status", "streamId"],
-                    include: [
-                        {
-                            model: db.course,
-                            as: 'preferred_courses',
-                            attributes: ["id", "course_name"],
-                            include: [
-                                {
-                                    model: db.campus,
-                                    through: "campus_course",
-                                    as: "campuses",
-                                    attributes: ["id","campus_name"],
-                                    through: {
-                                        attributes: ["course_link"]
-                                    }
-                                },
-                            ],
-                        },
-                        {
-                            model: db.stream,
-                            as: "preferred_stream",
-                            attributes: ["id", "stream_name"]
-                        },
-                        {
-                            model: db.campus,
-                            as: 'preferred_campus',
-                            attributes: ["id", "campus_name"]
-                        },
-                        {
-                            model: db.university,
-                            as: 'preferred_university',
-                            attributes: ["id", "university_name"]
-                        },
-                        {
-                            model: db.studyPreference,
-                            as: 'studyPreference',
-                            include: [
-                                {
-                                    model: db.country,
-                                    as: "country",
-                                    attributes: ["id", "country_name"]
-                                },
-                                {
-                                    model: db.userPrimaryInfo,
-                                    as: "userPrimaryInfo",
-                                    attributes: ["id", "full_name", "office_type", "source_id", "lead_received_date", "assign_type"],
-                                    include: [
-                                        {
-                                            model: db.leadSource,
-                                            required: true,
-                                            as: "source_name",
-                                            attributes: ["id", "source_name"]
-                                        },
-                                        {
-                                            model: db.officeType,
-                                            required: true,
-                                            as: "office_type_name",
-                                            attributes: ["id", "office_type_name"]
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ),
-        ])
-
-        return res.status(200).json({
-            status: true,
-            data: {
-                existApplication: existApplication,
-                studyPreferDetails: studyPreferDetails,
-                assigned_user: assigned_user
-            },
-            message: "Application Assigned to Team Member",
-        });
-
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        return res.status(500).json({
-            status: false,
-            message: error.message || "Internal server error",
-        });
+    if (!existApplication) {
+      throw new Error("Application not found");
     }
+
+    const [assigned_user, studyPreferDetails] = await Promise.all([
+      existApplication.getApplication({ attributes: ["id", "name"] }),
+      existApplication.getStudyPreferenceDetails({
+        attributes: ["id", "intakeYear", "intakeMonth","streamId"],
+        include: [
+          {
+            model: db.course,
+            as: "preferred_courses",
+            attributes: ["id", "course_name"],
+            include: [
+              {
+                model: db.campus,
+                through: "campus_course",
+                as: "campuses",
+                attributes: ["id", "campus_name"],
+                through: {
+                  attributes: ["course_link"],
+                },
+              },
+            ],
+          },
+          {
+            model: db.stream,
+            as: "preferred_stream",
+            attributes: ["id", "stream_name"],
+          },
+          {
+            model: db.campus,
+            as: "preferred_campus",
+            attributes: ["id", "campus_name"],
+          },
+          {
+            model: db.university,
+            as: "preferred_university",
+            attributes: ["id", "university_name"],
+          },
+          {
+            model: db.studyPreference,
+            as: "studyPreference",
+            include: [
+              {
+                model: db.country,
+                as: "country",
+                attributes: ["id", "country_name"],
+              },
+              {
+                model: db.userPrimaryInfo,
+                as: "userPrimaryInfo",
+                attributes: ["id", "full_name", "office_type", "source_id", "lead_received_date", "assign_type"],
+                include: [
+                  {
+                    model: db.leadSource,
+                    required: true,
+                    as: "source_name",
+                    attributes: ["id", "source_name"],
+                  },
+                  {
+                    model: db.officeType,
+                    required: true,
+                    as: "office_type_name",
+                    attributes: ["id", "office_type_name"],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      }),
+    ]);
+
+    return res.status(200).json({
+      status: true,
+      data: {
+        existApplication: existApplication,
+        studyPreferDetails: studyPreferDetails,
+        assigned_user: assigned_user,
+      },
+      message: "Application Assigned to Team Member",
+    });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    return res.status(500).json({
+      status: false,
+      message: error.message || "Internal server error",
+    });
+  }
 };
 
 exports.assignApplication = async (req, res, next) => {
@@ -206,25 +203,24 @@ exports.getApplicationDetailsByType = async (req, res, next) => {
             model: db.educationDetails,
             required: true,
             as: "educationDetails",
-            attributes: ["id", "qualification", "start_date", "end_date", "percentage", "board_name", "school_name"]
+            attributes: ["id", "qualification", "start_date", "end_date", "percentage", "board_name", "school_name"],
           },
           {
             model: db.graduationDetails,
             required: true,
             as: "graduationDetails",
-            attributes: ["id", "qualification", "start_date", "end_date", "percentage", "university_name", "college_name"]
+            attributes: ["id", "qualification", "start_date", "end_date", "percentage", "university_name", "college_name"],
           },
           {
             model: db.gapReason,
             required: true,
             as: "gapReasons",
-            attributes: ["id", "reason", "start_date", "end_date", "type"]
-          }
-        ]
+            attributes: ["id", "reason", "start_date", "end_date", "type"],
+          },
+        ],
       });
       response.data = educationalDetails;
     } else if (type == types.visa) {
-
       const visaDetails = await db.userPrimaryInfo.findByPk(id, {
         attributes: ["id"],
         include: [
@@ -237,10 +233,10 @@ exports.getApplicationDetailsByType = async (req, res, next) => {
               {
                 model: db.country,
                 required: true,
-                as: 'approved_country',
-                attributes: ["id", "country_name"]
-              }
-            ]
+                as: "approved_country",
+                attributes: ["id", "country_name"],
+              },
+            ],
           },
           {
             model: db.previousVisaDecline,
@@ -251,12 +247,12 @@ exports.getApplicationDetailsByType = async (req, res, next) => {
               {
                 model: db.country,
                 required: true,
-                as: 'declined_country',
-                attributes: ["id", "country_name"]
-              }
-            ]
-          }
-        ]
+                as: "declined_country",
+                attributes: ["id", "country_name"],
+              },
+            ],
+          },
+        ],
       });
       response.visaDetails = visaDetails;
     }
