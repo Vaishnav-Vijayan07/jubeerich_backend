feaee0c18a2e9b8dcc977a459134df398ec8ecd7
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-11-25 18:04:19 +0530

    added soft delete

--- models/university.js
+++ models/university.js
@@ -48,6 +48,11 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING(200),
       allowNull: true,
     },
+    is_active: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    }
   });
 
   return University;
