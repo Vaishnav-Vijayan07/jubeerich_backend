2895d33ac93e94912d6dbf9788bc36f9cd03d14b
afsalmern <afsal@intersmart.in>
2024-12-21 18:43:54 +0530

    added the functionalities for dasboard data for it team

--- controller/dashboard_controller.js
+++ controller/dashboard_controller.js
@@ -1,5 +1,7 @@
+const { QueryTypes } = require("sequelize");
 const IdsFromEnv = require("../constants/ids");
 const db = require("../models");
+const { leadStatusWiseCountQuery, leadStatusOfficeWiseCountQuery } = require("../raw/queries");
 
 exports.getDashboard = async (req, res) => {
   const { role_id } = req;
@@ -27,10 +29,17 @@ exports.getDashboard = async (req, res) => {
         });
     }
 
+    const { officeWiseCount, leadCount, officeTypes, statustyps, latestLeadsCount } = result;
+    const { statCards } = processCardData(leadCount);
+    const { categories, series } = transformOfficeWiseCountToChartData(officeWiseCount, officeTypes, statustyps);
+
     res.status(200).json({
       message: "Dashboard data fetched successfully",
       status: true,
-      result,
+      categories,
+      series,
+      statCards,
+      latestLeadsCount,
     });
   } catch (error) {
     console.error(error);
@@ -40,38 +49,113 @@ exports.getDashboard = async (req, res) => {
 
 const getDataForItTeam = async () => {
   try {
-    const { Op, literal } = db.Sequelize;
+    const officeTypes = await db.officeType.findAll({
+      attributes: ["office_type_name"],
+      raw: true,
+    });
 
-    const users = await db.userPrimaryInfo.findAll({
-      attributes: ["id", "full_name"], // Columns from userPrimaryInfo
-      where: { is_deleted: false },
+    const statustyps = await db.statusType.findAll({
+      attributes: ["type_name"],
+      raw: true,
+    });
+
+    const latestLeadsCount = await db.userPrimaryInfo.findAll({
+      attributes: ["id", "office_type", "stage","full_name"],
       include: [
         {
           model: db.status,
           as: "preferredStatus",
-          attributes: ["id", "status_name", "type_id"], // Columns from status
-          through: {
-            model: db.userContries,
-            attributes: [],
-          },
-          include: [
-            {
-              model: db.statusType,
-              as: "statusType",
-              attributes: ["priority", "type_name"], // Columns from statusType
-            },
-          ],
-          where: {
-            id: 1
-          }
+          attributes: ["status_name"],
+          through: {attributes: []}
         },
-
+        {
+          model: db.country,
+          as: "preferredCountries",
+          attributes: ["country_name"],
+          through: {attributes: []}
+        },
+        {
+          model: db.officeType,
+          as: "office_type_name",
+          attributes: ["office_type_name"],
+        },
       ],
+      limit: 6,
     });
 
-    return users;
+    const leadCount = await db.sequelize.query(leadStatusWiseCountQuery, {
+      type: QueryTypes.SELECT,
+    });
+
+    const officeWiseCount = await db.sequelize.query(leadStatusOfficeWiseCountQuery, {
+      type: QueryTypes.SELECT,
+    });
+
+    return { leadCount, officeWiseCount, officeTypes, statustyps, latestLeadsCount };
   } catch (error) {
     console.error("Error in getDataForItTeam:", error.message);
     throw new Error("Failed to fetch IT team data");
   }
+};
+
+const processCardData = (counts) => {
+  const totalLeads = counts[0]?.total_lead_count || 0;
+  // Define the base structure of the cards
+  const defaultCards = [
+    { id: 1, title: "total leads", stats: totalLeads, icon: "fe-users", bgColor: "#5bc0de" }, // Blue for general data
+    { id: 2, title: "spam leads", stats: "0", icon: "fe-alert-triangle", bgColor: "#f0ad4e" }, // Orange for warnings
+    { id: 3, title: "closed leads", stats: "0", icon: "fe-check-circle", bgColor: "#5cb85c" }, // Green for success
+    { id: 4, title: "prospective leads", stats: "0", icon: "fe-briefcase", bgColor: "#5bc0de" }, // Green for success
+    { id: 5, title: "failed Leads", stats: "0", icon: "fe-x-circle", bgColor: "#d9534f" }, // Red for errors
+  ];
+
+  const metaData = {
+    spam_leads: {
+      id: 2,
+      icon: "fe-alert-triangle",
+      bgColor: "#f0ad4e",
+    },
+    closed_leads: {
+      id: 3,
+      icon: "fe-check-circle",
+      bgColor: "#5cb85c",
+    },
+    prospective_leads: {
+      id: 4,
+      icon: "fe-check-circle",
+      bgColor: "#5cb85c",
+    },
+    failed_leads: {
+      id: 5,
+      icon: "fe-x-circle",
+      bgColor: "#d9534f",
+    },
+  };
+
+  let statCards = [...defaultCards];
+
+  counts.forEach((count) => {
+    const card = metaData[count.type_name];
+    if (card) {
+      statCards[card.id - 1].stats = count.count;
+    }
+  });
+
+  return { statCards };
+};
+
+const transformOfficeWiseCountToChartData = (officeWiseCount, officeTypes, statustyps) => {
+  const seriesMap = new Map(statustyps.map((type) => [type.type_name, []]));
+
+  officeWiseCount.forEach((item) => {
+    seriesMap.get(item.type_name).push(parseInt(item.count, 10));
+  });
+
+  const categories = officeTypes.map((office) => office.office_type_name);
+  const series = Array.from(seriesMap.entries()).map(([name, data]) => ({
+    name: name.replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase()),
+    data,
+  }));
+
+  return { categories, series };
 };
