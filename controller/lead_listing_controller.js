67185772ed1f416d9d11e3cebb46fd4c3d2698eb
afsalmern <afsal@intersmart.in>
2025-01-30 18:48:28 +0530

    completed the filters fuunctionality for cre tl and it team

--- controller/lead_listing_controller.js
+++ controller/lead_listing_controller.js
@@ -623,10 +623,7 @@ exports.getAllLeadsOptimized = async (req, res) => {
         is_deleted: false,
         [Op.and]: [
           {
-            [db.Op.or]: [
-              { full_name: { [db.Op.iLike]: dynamicIlike } },
-              { email : { [db.Op.iLike]: dynamicIlike }},
-            ],
+            [db.Op.or]: [{ full_name: { [db.Op.iLike]: dynamicIlike } }, { email: { [db.Op.iLike]: dynamicIlike } }],
           },
         ],
       };
@@ -665,10 +662,7 @@ exports.getAllLeadsOptimized = async (req, res) => {
             [Op.or]: [dynamicDependancy, { created_by: cre_id }, dynamicDependancyLiteral],
           },
           {
-            [db.Op.or]: [
-              { full_name: { [db.Op.iLike]: dynamicIlike } },
-              { email : { [db.Op.iLike]: dynamicIlike }},
-            ],
+            [db.Op.or]: [{ full_name: { [db.Op.iLike]: dynamicIlike } }, { email: { [db.Op.iLike]: dynamicIlike } }],
           },
         ],
       };
@@ -1097,10 +1091,34 @@ exports.getAllAssignedLeadsRegionalMangers = async (req, res) => {
 };
 
 exports.geLeadsForCreTl = async (req, res) => {
-  const { page = 1, limit = 20, keyword } = req.query;
+  const { page = 1, limit = 20, office = 0, country = 0, keyword, source = 0, sort_level = "DESC", sort_by = "id" } = req.query;
+
+  let officeWhere = {};
+  let countryWhere = {};
+  let sourceWhere = {};
+
+  if (office !== 0) {
+    officeWhere = {
+      id: office,
+    };
+  }
+
+  if (country !== 0) {
+    countryWhere = {
+      id: country,
+    };
+  }
+
+  if (source !== 0) {
+    sourceWhere = {
+      id: source,
+    };
+  }
 
   const dynamicIlike = keyword ? `%${keyword}%` : `%%`;
   const isSearchApplied = keyword ? true : false;
+  const isSortApplied = sort_level !== "DESC" && sort_by !== "id";
+  const sortOrder = [[sort_by, sort_level.toUpperCase()]];
 
   const offset = (page - 1) * limit;
   const parsedLimit = parseInt(limit, 10);
@@ -1148,33 +1166,18 @@ exports.geLeadsForCreTl = async (req, res) => {
           },
         ],
       },
-      // distint: true,
       include: [
-        // {
-        //   model: db.leadCategory,
-        //   as: "category_name",
-        //   attributes: ["category_name"],
-        // },
         {
           model: db.leadSource,
           as: "source_name",
           attributes: ["source_name"],
+          where: sourceWhere,
         },
-        {
-          model: db.leadChannel,
-          as: "channel_name",
-          attributes: ["channel_name"],
-        },
-        // {
-        //   model: db.country,
-        //   as: "preferredCountries", // Use the alias defined in associations
-        //   attributes: ["country_name", "id"], // Include both name and ID
-        //   through: { attributes: [] }, // Exclude attributes from join table
-        // },
         {
           model: db.country,
           as: "preferredCountries",
           attributes: ["id", "country_name"],
+          where: countryWhere,
           through: {
             model: db.userContries,
             attributes: ["country_id", "followup_date", "status_id"],
@@ -1199,12 +1202,7 @@ exports.geLeadsForCreTl = async (req, res) => {
           as: "office_type_name",
           attributes: ["office_type_name"],
         },
-        {
-          model: db.region,
-          as: "region_name",
-          attributes: ["region_name"],
-          required: false,
-        },
+
         {
           model: db.adminUsers,
           as: "counsiler_name",
@@ -1216,12 +1214,7 @@ exports.geLeadsForCreTl = async (req, res) => {
           as: "cre_name",
           attributes: ["id", "name"],
         },
-        {
-          model: db.branches,
-          as: "branch_name",
-          attributes: ["branch_name"],
-          required: false,
-        },
+
         {
           model: db.adminUsers,
           as: "updated_by_user",
@@ -1229,28 +1222,10 @@ exports.geLeadsForCreTl = async (req, res) => {
           required: false,
           foreignKey: "updated_by",
         },
-        {
-          model: db.status,
-          as: "status",
-          attributes: ["status_name"],
-          required: false,
-        },
-        // {
-        //   model: db.flag,
-        //   as: "user_primary_flags",
-        //   attributes: ["flag_name"],
-        //   required: false,
-        // },
-        {
-          model: db.userExams,
-          as: "exams",
-          // attributes: ["exam_name","marks", "document"],
-          required: false,
-        },
       ],
       offset,
       limit: parsedLimit,
-      order: [["id", "DESC"]],
+      order: sortOrder
     });
 
     const formattedUserPrimaryInfos = await Promise.all(
@@ -1264,47 +1239,16 @@ exports.geLeadsForCreTl = async (req, res) => {
           followup_date: country.user_countries?.followup_date,
         }));
 
-        // const examDetails = info.exams.map((exam)=> ({
-        //   exam_name: exam.exam_name,
-        //   marks: exam.marks,
-        // }))
-
-        const examDetails = info.exams.map((exam) => ({
-          exam_type: exam.exam_type,
-          exam_date: exam.exam_date,
-          marks: exam.overall_score,
-          listening_score: exam.listening_score,
-          speaking_score: exam.speaking_score,
-          reading_score: exam.reading_score,
-          writing_score: exam.writing_score,
-          updated_by: exam.updated_by,
-        }));
-
-        const examDocuments = info.exams.map((exam) => ({
-          exam_documents: exam.score_card,
-        }));
-
-        const flagDetails = await info.flag_details;
-
         return {
           ...info.toJSON(),
-          // category_name: info.category_name
-          //   ? info.category_name.category_name
-          //   : null,
           source_name: info.source_name ? info.source_name.source_name : null,
-          channel_name: info.channel_name ? info.channel_name.channel_name : null,
           preferredCountries: preferredCountries,
-          franchise_id: info.franchise_id ? info.franchise_id : null,
           office_type_name: info.office_type_name ? info.office_type_name.office_type_name : null,
           region_name: info.region_name ? info.region_name.region_name : null,
           counsiler_name: info.counsiler_name ? info.counsiler_name.name : null,
-          branch_name: info.branch_name ? info.branch_name.branch_name : null,
           cre_name: info.cre_name ? info.cre_name.name : "Not assigned", // Added cre_name extraction
           updated_by_user: info.updated_by_user ? info.updated_by_user.name : null,
           status: info.status ? info.status.status_name : null,
-          exam_details: examDetails,
-          exam_documents: examDocuments,
-          flag_details: flagDetails,
           isDeleteEnabled: getDeleteCondition(roleId, info, userId),
         };
       })
